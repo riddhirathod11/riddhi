3eb509604112f6480472408846781cb96edb0f5d
riddhirathod11 <riddhiriddhirathod@gmail.com>
2026-07-23 10:54:39 +0530

    Add files via upload

---
+++ module/arithmeticopex.dart
@@ -0,0 +1,21 @@
+void main()
+{
+    int a=6;
+    int b=3;
+
+   var add = a+b;
+   print(add);
+
+   var sub = a-b;
+   print(sub);
+
+   var mul = a*b;
+   print(mul);
+
+   var div = a/b;
+   print (div);
+
+   var mod =a%b;
+   print (mod);
+}
+
